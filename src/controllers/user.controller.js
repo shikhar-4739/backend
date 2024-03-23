1a75c8118c1cef7772d1c39b61ff5eebf625dc79
Shikhar <shikhar.varshney2003@gmail.com>
2024-03-23 18:02:22 +0530

    register controller bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,7 +5,7 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 
 const registerUser = asyncHandler( async (req, res) => {
-    
+
     // get user details feom frontend
     // validate for non-empty
     // check if user already exists: username, email
@@ -24,7 +24,7 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new ApiError(400, "All fields are required")
     }
 
-    const existedUser = User.findOne({                     // checking for existing User
+    const existedUser = await User.findOne({                     // checking for existing User
         $or: [{ username }, { email }]
     })
 
@@ -33,7 +33,11 @@ const registerUser = asyncHandler( async (req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0 ) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file is required");
